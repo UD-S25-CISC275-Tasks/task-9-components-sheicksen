ab89889dd4c40dc36ca9ef1e7a38b9dd18c6e1d3
Sophia Heicksen <heicksen@udel.edu>
2025-03-06 20:15:44 -0500

    Made functional CycleHoliday component

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,46 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+// Chinese NY, St. Patties Day, Halloween, Krampus Day, Christmas
+type holidays = "🐉" | "🍀" | "🎃" | "🐐" | "🎄";
+const holidayTime: Record<holidays, holidays> = {
+    "🐉": "🍀",
+    "🍀": "🎃",
+    "🎃": "🐐",
+    "🐐": "🎄",
+    "🎄": "🐉",
+};
+const holidayAlph: Record<holidays, holidays> = {
+    "🐉": "🎄",
+    "🎄": "🎃",
+    "🎃": "🐐",
+    "🐐": "🍀",
+    "🍀": "🐉",
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<holidays>("🎃");
+    return (
+        <div>
+            Cycle Holiday
+            <br></br>
+            <Button
+                onClick={() => {
+                    setHoliday(holidayTime[holiday]);
+                }}
+            >
+                Advance by Time
+            </Button>
+            <Button
+                onClick={() => {
+                    setHoliday(holidayAlph[holiday]);
+                }}
+            >
+                Advance by Alphabet
+            </Button>
+            <br />
+            Holiday:
+            <span style={{ fontSize: "60px" }}>{holiday}</span>
+        </div>
+    );
 }
